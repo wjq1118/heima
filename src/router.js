74f7736d1137dce5435fe39e08a8612848017d39
jiaqing wan <wjq>
2019-09-19 15:41:33 +0800

    登陆模块-页面布局及样式

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue' // 路由级组件
-
+import Login from './views/login'
 Vue.use(Router)
 
 export default new Router({
@@ -9,6 +9,10 @@ export default new Router({
     {
       path: '/',
       component: Home
+    },
+    {
+      path: '/login',
+      component: Login
     }
     // {
     //   path: '/about',
---
+++ src/views/login/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="login">
+    <!-- 使用elementUI组件 el-card -->
+    <el-card class="login-card">
+      <div  class="title">
+        <img src="../../assets/logo_index.png" alt />
+      </div>
+      <!-- 表单=>elform包裹 -->
+      <el-form style="margin-top:20px">
+        <el-form-item>
+          <!-- 手机号 -->
+          <el-input></el-input>
+        </el-form-item>
+        <el-form-item>
+          <!-- 验证码 -->
+          <el-input style="width:60%"></el-input>
+          <!-- 发送验证码 -->
+          <el-button style="float:right ">发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+            <!-- 登录按钮 -->
+            <el-button type="primary"  style="width:100%">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+// 如果要在组件样式中写less,就要给一个lang属性   lang='less'
+.login {
+  background-image: url(../../assets/login_bg.jpg);
+  height: 100vh;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .login-card {
+    width: 440px;
+    height: 340px;
+    .title {
+      text-align: center;
+      img {
+        height: 45px;
+      }
+    }
+  }
+}
+</style>
