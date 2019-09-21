0bb4ee993e8f51fcbe2463f4868cc9caf1efea19
jiaqing wan <wjq>
2019-09-21 21:32:24 +0800

    安装axios

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui' // 引入UI
+import axios from 'axios'
 import 'element-ui/lib/theme-chalk/index.css' //
 import './styles/index.less'
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0' // 将地址的常态值设置给baseUrl
+Vue.prototype.$axios = axios // 将axios共享给所有Vue或者组件实例使用
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 new Vue({
