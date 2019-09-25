a82858169a7c42a84797b2124e89c7eaa9afc8c0
jiaqing wan <wjq>
2019-09-25 17:07:25 +0800

    头部结构和下拉菜单

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,12 +2,14 @@
 <!-- 先定义一个大容器 -->
     <el-container>
         <!-- 先放置一个左侧 -->
-        <el-aside>
+        <el-aside style="width:200px">
             <layout-aside></layout-aside>
         </el-aside>
         <!-- 右侧 -->
         <el-container>
-            <el-header>头部</el-header>
+            <el-header>
+                <layout-header></layout-header>
+            </el-header>
             <!-- 中部区域 -->
             <el-main>
                 <router-view></router-view>
@@ -16,8 +18,13 @@
     </el-container>
 </template>
 <script>
+import layoutAside from '../../components/home/layout-aside'
+import layoutHeader from '../../components/home/layout-header'
 export default {
-
+  components: {
+    'layout-aside': layoutAside,
+    'layout-header': layoutHeader
+  }
 }
 </script>
 
